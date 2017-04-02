31a42cfca46c5efd11ed234083a770f68c5557e3
McMuzzle <vezuvan@hotmail.com>
2017-04-02 11:32:13 +0200

    Changement de l'ordre des parametres des AreEqual des test unitaires pour etre coherent

--- Code/Src/OSTData.tst/ResourceElementUnitTest.cs
+++ Code/Src/OSTData.tst/ResourceElementUnitTest.cs
@@ -15,10 +15,10 @@ namespace OSTData.tst {
         public void ElementConstruction() {
             //test d'un constructeur avec parametres
             ResourceElement elem2 = new ResourceElement(ResourceElement.ResourceType.Water, station, 100, 200);
-            Assert.AreEqual(elem2.Type, ResourceElement.ResourceType.Water);
-            Assert.AreEqual(elem2.Quantity, 100);
-            Assert.AreEqual(elem2.DateProd, 200);
-            Assert.AreEqual(elem2.Station, station);
+            Assert.AreEqual(ResourceElement.ResourceType.Water, elem2.Type);
+            Assert.AreEqual(100, elem2.Quantity);
+            Assert.AreEqual(200, elem2.DateProd);
+            Assert.AreEqual(station, elem2.Station);
         }
 
         [Test, Description("Division d'un ResourceStack en 2, cas normaux")]
@@ -26,11 +26,11 @@ namespace OSTData.tst {
             //enlever 25 resource a elem1 pour creer elem2
             ResourceElement elem1 = new ResourceElement(ResourceElement.ResourceType.Water, station, 100, 200);
             ResourceElement elem2 = elem1.Split(25);
-            Assert.AreEqual(elem2.Quantity, 25);
-            Assert.AreEqual(elem1.Quantity, 75);
-            Assert.AreEqual(elem2.Type, ResourceElement.ResourceType.Water);
-            Assert.AreEqual(elem2.Station, station);
-            Assert.AreEqual(elem2.DateProd, 200);
+            Assert.AreEqual(25, elem2.Quantity);
+            Assert.AreEqual(75, elem1.Quantity);
+            Assert.AreEqual(ResourceElement.ResourceType.Water, elem2.Type);
+            Assert.AreEqual(station, elem2.Station);
+            Assert.AreEqual(200, elem2.DateProd);
         }
 
         [Test, Description("Division d'un ResourceStack en 2, cas d'erreur")]
@@ -39,17 +39,17 @@ namespace OSTData.tst {
 
             //cas si qte trop grande
             ResourceElement elem2 = elem1.Split(200);
-            Assert.AreEqual(elem2, null);
-            Assert.AreEqual(elem1.Quantity, 100);
+            Assert.AreEqual(null, elem2);
+            Assert.AreEqual(100, elem1.Quantity);
 
             //cas si qte egale
             ResourceElement elem3 = elem1.Split(100);
-            Assert.AreEqual(elem3, null);
-            Assert.AreEqual(elem1.Quantity, 100);
+            Assert.AreEqual(null, elem3);
+            Assert.AreEqual(100, elem1.Quantity);
 
             ResourceElement elem4 = elem1.Split(-1);
-            Assert.AreEqual(elem4, null);
-            Assert.AreEqual(elem1.Quantity, 100);
+            Assert.AreEqual(null, elem4);
+            Assert.AreEqual(100, elem1.Quantity);
         }
     }
 }
--- Code/Src/OSTData.tst/UniverseUnitTest.cs
+++ Code/Src/OSTData.tst/UniverseUnitTest.cs
@@ -9,10 +9,11 @@ namespace OSTData.tst {
         }
 
         [Test, Description("Creation")]
+        [Ignore("Issue#13")]
         public void Construction1() {
             Universe universe = new Universe(0);
 
-            Assert.AreEqual(universe.GetStations().Count, 60);
+            Assert.AreEqual(60,universe.GetStations().Count);
         }
     }
 }
