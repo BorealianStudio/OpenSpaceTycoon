aca4bd55ef64796d3c43ee5f87a38e897605a4a0
mcmuzzle <vezuvan@hotmail.com>
2017-06-13 10:18:13 +0200

    correction du probleme post build sous travis

--- Code/Solutions/OpenSpaceTycoon2015/buildCopy.bat
+++
@@ -1,3 +0,0 @@
-if exist $(SolutionDir)\..\..\..\..\OpenSpaceTycoon.ClientUnity\OpenSpaceTycoonClient\Assets\ThirdParty\Model\Plugins (
-copy "$(TargetDir)*.dll" "$(SolutionDir)\..\..\..\..\OpenSpaceTycoon.ClientUnity\OpenSpaceTycoonClient\Assets\ThirdParty\Model\Plugins"
-)
